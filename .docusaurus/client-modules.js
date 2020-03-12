1110df84c4467363b0294402e5de58b76debd966
onr <onur.akpolat@gmail.com>
2020-03-12 11:18:37 +0100

    Deploy website version based on 2bf99138032f7dd34cf9b0cbf9a1c39224235f30

--- .docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/home/onur/Downloads/test/radicle-run/src/css/custom.css"),
-];
